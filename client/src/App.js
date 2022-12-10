9b440d7550be6495e13e3dfb99d4ac7699a96ad2
Anna An <arzumanyan.an@gmail.com>
2022-12-10 11:14:27 +0400

    adding React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,32 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
-  return <div className="App">Something here!</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
