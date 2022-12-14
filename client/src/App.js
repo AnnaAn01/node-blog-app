13617013380750f8e24d245621473b5c93ebcc37
Anna An <arzumanyan.an@gmail.com>
2022-12-14 15:02:53 +0400

    styling login page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Write from "./pages/Write";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import "./style.scss";
 
 const Layout = () => {
   return (
