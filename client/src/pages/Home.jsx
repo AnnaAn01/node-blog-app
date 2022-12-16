d41280c1c1da84a6ee341d228ecc4dcf2ac361c6
Anna An <arzumanyan.an@gmail.com>
2022-12-16 12:40:56 +0400

    styling home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -44,9 +44,8 @@ const Home = () => {
                             <img src={post.img} alt="" />
                         </div>
                         <div className="content">
-                            <Link to={`/post/${post.id}`}>
+                            <Link className='link' to={`/post/${post.id}`}>
                                 <h1>{post.title}</h1>
-
                             </Link>
                             <p>{post.desc}</p>
                             <button>Read more</button>
