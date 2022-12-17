c7fb5499c0b90667a57024508fe1e52e768145cf
Anna An <arzumanyan.an@gmail.com>
2022-12-17 10:36:16 +0400

    styling individual page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Single = () => {
     return (
         <div className='single'>
-            <div className="content">c</div>
+            <div className="content">
+                <img src="https://images.pexels.com/photos/7990901/pexels-photo-7990901.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <div className="user">
+                    <img src="https://images.pexels.com/photos/1264210/pexels-photo-1264210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <div className="info">
+                        <span>John</span>
+                        <p>Posted 2 days ago</p>
+                    </div>
+                </div>
+            </div>
             <div className="menu">m</div>
         </div>
     )
