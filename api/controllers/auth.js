7aacb0f05099d4b198199e4c7c242a2379957da2
Anna An <arzumanyan.an@gmail.com>
2023-01-05 09:44:42 +0400

    logn functionality

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -25,6 +25,22 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  // CHECK USER
+  const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [req, body, username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+  });
+
+  // CHECK PASSWORD
+  // data retuns an array, so we'll use data[0] to select the first item, which is our user
+  const isPasswordCorrect = bcrypt.compareSync(
+    req.body.password,
+    data[0].password
+  );
+  if (!isPasswordCorrect)
+    return res.status(400).json("Wrong username or password!");
+};
 
 export const logout = (req, res) => {};
