6c907f6a833f5f30e0eb1fd790c1b49ba9755bbb
Anna An <arzumanyan.an@gmail.com>
2023-01-11 15:58:29 +0400

    adding upload image function

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
@@ -12,6 +13,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+// for oploading files in your own server using multer
+const upload = multer({ dest: "uploads/" });
+
+app.post("/upload", upload.single("file"), function (req, res) {
+  res.status(200).json("Image has been uploaded");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
